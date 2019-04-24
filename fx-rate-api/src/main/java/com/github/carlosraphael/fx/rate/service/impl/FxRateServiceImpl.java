9d00ea7d5cb5872df89174de5d14777d345a10aa
Carlos <crlp.ireland@gmail.com>
2019-04-24 22:12:45 +0100

    fixed spring cache thing

--- fx-rate-api/src/main/java/com/github/carlosraphael/fx/rate/service/impl/FxRateServiceImpl.java
+++ fx-rate-api/src/main/java/com/github/carlosraphael/fx/rate/service/impl/FxRateServiceImpl.java
@@ -4,44 +4,32 @@ import com.github.carlosraphael.fx.rate.domain.FxRate;
 import com.github.carlosraphael.fx.rate.service.FxRateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriTemplate;
-import reactor.cache.CacheMono;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.Signal;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.Currency;
 import java.util.Map;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class FxRateServiceImpl implements FxRateService {
 
-    static final String CACHE_NAME = "rates";
-
     private final WebClient webClient;
-    private final CacheManager cacheManager;
 
     @Value("${rate.api.URI}")
     private UriTemplate uriTemplate;
 
     @Override
+    @Cacheable(cacheNames = "rates", key = "{#source.currencyCode, #target.currencyCode}")
     public Mono<FxRate> getRate(Currency source, Currency target) {
         String currencyQuery = source.getCurrencyCode() + "_" + target.getCurrencyCode();
 
-        return CacheMono.lookup(key -> Mono.justOrEmpty(getCachedRate(key)).map(Signal::next), currencyQuery)
-                .onCacheMissResume(() -> fetchRemoteRateApi(source, target, currencyQuery))
-                .andWriteWith((key, signal) -> Mono.fromRunnable(() -> getCache().put(key, signal.get())));
-    }
-
-    private Mono<FxRate> fetchRemoteRateApi(Currency source, Currency target, String currencyQuery) {
         return webClient.get().uri(uriTemplate.expand(currencyQuery).toString())
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
@@ -52,14 +40,6 @@ public class FxRateServiceImpl implements FxRateService {
                         .rate(new BigDecimal(rate.get(currencyQuery).toString()))
                         .time(OffsetDateTime.now())
                         .build()
-                );
-    }
-
-    private FxRate getCachedRate(String key) {
-        return (FxRate) getCache().get(key);
-    }
-
-    private Map<Object, Object> getCache() {
-        return ((CaffeineCache) Objects.requireNonNull(cacheManager.getCache(CACHE_NAME))).getNativeCache().asMap();
+                ).cache();
     }
 }
--- fx-rate-api/src/test/java/com/github/carlosraphael/fx/rate/service/FxRateServiceTest.java
+++ fx-rate-api/src/test/java/com/github/carlosraphael/fx/rate/service/FxRateServiceTest.java
@@ -11,11 +11,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.io.BufferedReader;
@@ -28,6 +30,8 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @EnableCaching
@@ -36,8 +40,7 @@ import static org.assertj.core.api.Assertions.assertThat;
         "rate.api.URI=/api/v7/convert?q={currencyQuery}&compact=ultra&apiKey=API_KEY",
         "rate.api.baseURL=http://localhost:24596",
         "spring.cache.cache-names=rates",
-        "spring.cache.caffeine.spec=maximumSize=500,expireAfterWrite=60s",
-        "spring.cache.type=CAFFEINE"
+        "spring.cache.caffeine.spec=maximumSize=500,expireAfterWrite=60s"
 })
 @SpringBootTest
 public class FxRateServiceTest {
@@ -46,6 +49,8 @@ public class FxRateServiceTest {
 
     @Autowired
     FxRateService fxRateService;
+    @SpyBean
+    WebClient webClient;
 
     @Before
     public void setup() throws IOException {
@@ -60,33 +65,36 @@ public class FxRateServiceTest {
 
     @Test
     public void shouldReturnFxRateWhenSourceCurrencyIsEURAndTargetCurrencyIsUSD() {
+        // given
         Currency sourceCurrency = Currency.getInstance("EUR");
         Currency targetCurrency = Currency.getInstance("USD");
+        prepareResponse(response -> response.setResponseCode(200).setBody(basedOn("json/EUR_USD.json")));
 
-        prepareResponse(response -> response.setResponseCode(200)
-                .setHeader("Content-Type", "application/json").setBody(basedOn("json/EUR_USD.json")));
-
+        // when
         Mono<FxRate> fxRateMono = fxRateService.getRate(sourceCurrency, targetCurrency);
 
-        assertThat(fxRateMono).isNotNull();
+        // then
         assertThat(fxRateMono.block())
                 .isNotNull()
                 .extracting("rate", "source", "target")
                 .containsOnly(new BigDecimal("1.124483"), sourceCurrency, targetCurrency);
+        verify(webClient, times(1)).get();
     }
 
     @Test
     public void shouldReturnCachedFxRateWhenServiceIsCalledForTheSecondTime() {
+        // given
         Currency sourceCurrency = Currency.getInstance("USD");
         Currency targetCurrency = Currency.getInstance("EUR");
-
-        prepareResponse(response -> response.setResponseCode(200).setBody(basedOn("json/USD_EUR.json")));
         prepareResponse(response -> response.setResponseCode(200).setBody(basedOn("json/USD_EUR.json")));
 
+        // when
         Mono<FxRate> fxRateMonoFirstCall = fxRateService.getRate(sourceCurrency, targetCurrency);
-        Mono<FxRate> fxRateMonoSecondCall = fxRateMonoFirstCall.doAfterTerminate(() -> fxRateService.getRate(sourceCurrency, targetCurrency));
+        Mono<FxRate> fxRateMonoSecondCall = fxRateService.getRate(sourceCurrency, targetCurrency);
 
+        // then
         assertThat(fxRateMonoFirstCall.block()).isSameAs(fxRateMonoSecondCall.block());
+        verify(webClient, times(1)).get();
     }
 
     private void prepareResponse(Consumer<MockResponse> consumer) {
